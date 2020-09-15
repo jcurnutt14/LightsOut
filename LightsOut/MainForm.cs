cce8fdabd380777bcec74e4fa6c756bb5c7b9f72
jcurnutt14 <38479502+jcurnutt14@users.noreply.github.com>
2020-09-14 20:42:33 -0500

    test changes

--- LightsOut/MainForm.cs
+++ LightsOut/MainForm.cs
@@ -15,22 +15,17 @@ namespace LightsOut
 
         private const int GridOffset = 25;      // Distance from upper-left side of window
         private const int GridLength = 200;     // Size in pixels of grid
-        private const int NumCells = 3;         // Number of cells in grid
+        //private const int NumCells = 3;         // Number of cells in grid
         private const int CellLength = GridLength / NumCells;
 
-        private bool[,] grid;                   // Stores on/off state of cells in grid
-        private Random rand;                    // Used to generate random numbers
+        //private bool[,] grid;                   // Stores on/off state of cells in grid
+        //private Random rand;                    // Used to generate random numbers
         public MainForm()
         {
             InitializeComponent();
 
-            rand = new Random();
-            grid = new bool[NumCells, NumCells];
-
-            //Turn entire grid on
-            for (int r = 0; r < NumCells; r++)
-                for (int c = 0; c < NumCells; c++)
-                    grid[r, c] = true;
+            lightsOutGame = new LightsOutGame();
+           
         }
 
         private void gameToolStripMenuItem_Click(object sender, EventArgs e)
@@ -141,4 +136,102 @@ namespace LightsOut
             aboutBox.ShowDialog(this);
         }
     }
+
+    class LightsOutGame
+    {
+        private int gridSize = 3;               // Number of cells in grid
+        private bool[,] grid;                   // Stores on/off state of cells in grid
+        private Random rand;		        // Used to generate random numbers
+
+        public const int MaxGridSize = 7;
+        public const int MinGridSize = 3;
+
+        // Returns the number of horizontal/vertical cells in the grid
+        public int GridSize
+        {
+            get
+            {
+                return gridSize;
+            }
+            set
+            {
+                if (value >= MinGridSize && value <= MaxGridSize)
+                {
+                    gridSize = value;
+                    grid = new bool[gridSize, gridSize];
+                    NewGame();
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("NumCells must be between " +
+                        MinGridSize + " and " + MaxGridSize + ".");
+                }
+            }
+        }
+
+        public LightsOutGame()
+        {
+            rand = new Random();    // Initialize random number generator
+
+            GridSize = MinGridSize;
+        }
+
+        // Returns the grid value at the given row and column
+        public bool GetGridValue(int row, int col)
+        {
+            return grid[row, col];
+        }
+
+        // Randomizes the grid
+        public void NewGame()
+        {
+            // Fill grid with either white or black
+            for (int r = 0; r < gridSize; r++)
+            {
+                for (int c = 0; c < gridSize; c++)
+                {
+                    grid[r, c] = rand.Next(2) == 1;
+                }
+            }
+        }
+
+        // Inverts the selected box and all surrounding boxes
+        public void Move(int row, int col)
+        {
+            if (row < 0 || row >= gridSize || col < 0 || col >= gridSize)
+            {
+                throw new ArgumentException("Row or column is outside the legal range of 0 to "
+                    + (gridSize - 1));
+            }
+
+            for (int i = row - 1; i <= row + 1; i++)
+            {
+                for (int j = col - 1; j <= col + 1; j++)
+                {
+                    if (i >= 0 && i < gridSize && j >= 0 && j < gridSize)
+                    {
+                        grid[i, j] = !grid[i, j];
+                    }
+                }
+            }
+        }
+
+        // Returns true if all cells are off
+        public bool IsGameOver()
+        {
+            for (int r = 0; r < gridSize; r++)
+            {
+                for (int c = 0; c < gridSize; c++)
+                {
+                    if (grid[r, c])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+
 }
